f7d21336981ddaef493eef6d1bf2e23e47ba0c59
SamuelWakoli <swwakoli@gmail.com>
2023-04-28 20:32:18 +0300

    design for purchased products at the account page

--- lib/pages/purchased_products.dart
+++ lib/pages/purchased_products.dart
@@ -8,12 +8,33 @@ class PurchasedProducts extends StatefulWidget {
 }
 
 class _PurchasedProductsState extends State<PurchasedProducts> {
+  Widget listItem() {
+    return ListTile(
+      leading: ClipRRect(
+        borderRadius: BorderRadius.circular(10),
+        child: SizedBox(
+          height: 120,
+          width: 120,
+          child: Image.asset(
+            "assets/item.jpg",
+            fit: BoxFit.fill,
+          ),
+        ),
+      ),
+      title: Text("Product Name"),
+      subtitle: Text("Quantity: 1\nAmount paid: Ksh. 2000\nDate: 12/12/2022"),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         title: const Text("Purchased Products"),
+      ),
+      body: ListView(
+        children: [listItem()],
       ),
     );
   }
