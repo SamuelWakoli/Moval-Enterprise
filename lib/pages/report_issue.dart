35227f456952542cd36088d0917121135af11d01
SamuelWakoli <swwakoli@gmail.com>
2023-04-28 20:33:40 +0300

    design for the report issue

--- lib/pages/report_issue.dart
+++ lib/pages/report_issue.dart
@@ -8,12 +8,93 @@ class ReportIssue extends StatefulWidget {
 }
 
 class _ReportIssueState extends State<ReportIssue> {
+  String? _issue;
+  bool loading = false;
+
+  InputBorder textFieldBorder = OutlineInputBorder(
+    borderRadius: BorderRadius.circular(10.0),
+  );
+
+  sendIssue() {
+    if (_globalKey.currentState!.validate()) {
+      //TODO: Code for sending issue
+    }
+  }
+
+  final GlobalKey<FormState> _globalKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         title: const Text("Report Issue"),
+      ),
+      body: Form(
+        key: _globalKey,
+        child: SingleChildScrollView(
+          child: ConstrainedBox(
+              constraints: const BoxConstraints(
+                maxWidth: 400.0,
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    TextFormField(
+                      keyboardType: TextInputType.emailAddress,
+                      textInputAction: TextInputAction.send,
+                      autofocus: true,
+                      minLines: 10,
+                      maxLines: 10,
+                      decoration: InputDecoration(
+                        hintText: "Type your issue here",
+                        labelStyle:
+                            TextStyle(color: Theme.of(context).primaryColor),
+                        enabledBorder: textFieldBorder,
+                        focusedBorder: textFieldBorder,
+                        errorBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: const BorderSide(color: Colors.red)),
+                        focusedErrorBorder: textFieldBorder,
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Email required';
+                        }
+                        return null;
+                      },
+                      onFieldSubmitted: (value) {
+                        _issue = value;
+                        sendIssue();
+                      },
+                    ),
+                    const SizedBox(height: 16.0),
+                    SizedBox(
+                        width: double.infinity,
+                        child: OutlinedButton(
+                            onPressed: () async {
+                              sendIssue();
+                            },
+                            child: loading
+                                ? SizedBox(
+                                    height: 20,
+                                    width: 20,
+                                    child: CircularProgressIndicator(
+                                      color: Theme.of(context).primaryColor,
+                                    ),
+                                  )
+                                : Text(
+                                    "Send",
+                                    style: TextStyle(
+                                        color: Theme.of(context).primaryColor),
+                                  ))),
+                  ],
+                ),
+              )),
+        ),
       ),
     );
   }
